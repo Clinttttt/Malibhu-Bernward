fc08a33bb6a9c19d5e40c8edfb8f07e705319248
Clint Villanueva <clintvillanueva82@gmail.com>
2026-05-20 03:50:35 +0800

    Fix: Hide navigation links on first run until admin setup is complete

--- includes/header.php
+++ includes/header.php
@@ -28,6 +28,20 @@
 
     <div class="nav-links">
 
+        <?php 
+        // Check if admin is configured
+        $admin_configured = file_exists(
+            (strpos($_SERVER['PHP_SELF'], '/user/') !== false || strpos($_SERVER['PHP_SELF'], '/admin/') !== false) 
+            ? '../config/admin.txt' 
+            : 'config/admin.txt'
+        );
+        
+        // Only show navigation if admin is configured OR we're on setup page
+        $on_setup_page = strpos($_SERVER['PHP_SELF'], 'setup.php') !== false;
+        
+        if($admin_configured || $on_setup_page):
+        ?>
+
         <a href="<?php echo strpos($_SERVER['PHP_SELF'], '/user/') !== false || strpos($_SERVER['PHP_SELF'], '/admin/') !== false ? '../' : ''; ?>index.php">
             Home
         </a>
@@ -52,6 +66,8 @@
         <a href="<?php echo strpos($_SERVER['PHP_SELF'], '/user/') !== false || strpos($_SERVER['PHP_SELF'], '/admin/') !== false ? '' : 'user/'; ?>register.php">
             Register
         </a>
+        <?php endif; ?>
+        
         <?php endif; ?>
 
     </div>
